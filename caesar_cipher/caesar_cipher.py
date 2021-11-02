78a39a564d9d4669a8c1ef4ff95ec1474409fb32
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-11-02 16:21:57 +0200

    lab 18

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -34,16 +34,12 @@ def decrypt(encrypted,key):
     return encrypt(encrypted,-key)
 
 def count_words(text):
-
     words = text.split()
-
     word_count = 0
-
     for candidate_word in words:
         word = re.sub(r'[^A-Za-z]+','', candidate_word)
         if word.lower() in word_list or word in name_list:
             word_count += 1
-
     return word_count
 
 def crack(phrase): 
@@ -51,5 +47,5 @@ def crack(phrase):
         new_phrase = decrypt(phrase,key)
         word_count = count_words(new_phrase)
         percentage = int(word_count / len(phrase.split()) * 100)
-        if percentage > 50:
-            return(new_phrase)    
+        if percentage > 90:
+            return(new_phrase)   
